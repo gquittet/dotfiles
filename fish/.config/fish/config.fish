e27a64ab1e38321611ee2f10c9caa886946e5a87
Guillaume Quittet <guillaume.quittet@gmail.com>
2020-03-22 17:27:53 +0100

    Added specific macOS env var

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,7 +14,7 @@ set -g fish_emoji_width 2
 set fish_greeting ""
 
 # Fish syntax highlighting
-set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_autosuggestion '555' 'brblack'
 set -g fish_color_cancel -r
 set -g fish_color_command --bold
 set -g fish_color_comment red
@@ -22,7 +22,7 @@ set -g fish_color_cwd green
 set -g fish_color_cwd_root red
 set -g fish_color_end brmagenta
 set -g fish_color_error brred
-set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_escape 'bryellow' '--bold'
 set -g fish_color_history_current --bold
 set -g fish_color_host normal
 set -g fish_color_match --background=brblue
@@ -31,8 +31,8 @@ set -g fish_color_operator bryellow
 set -g fish_color_param cyan
 set -g fish_color_quote yellow
 set -g fish_color_redirection brblue
-set -g fish_color_search_match 'bryellow'  '--background=brblack'
-set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_search_match 'bryellow' '--background=brblack'
+set -g fish_color_selection 'white' '--bold' '--background=brblack'
 set -g fish_color_user brgreen
 set -g fish_color_valid_path --underline
 
@@ -65,11 +65,11 @@ alias cat 'bat --theme=base16'
 alias ls exa
 
 if test (uname) = "Darwin"
-	set -g fish_user_paths /usr/local/opt/node@12/bin $fish_user_paths
-	set -x FLUTTERPATH $HOME/Library/flutter
-	set -x GOPATH $HOME/Documents/Projets/go
-	set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-13.0.2.jdk/Contents/Home
-	set PATH $FLUTTERPATH/bin $GOPATH/bin $PATH
+    set -g fish_user_paths /usr/local/opt/node@12/bin $fish_user_paths
+    set -x FLUTTERPATH $HOME/Library/flutter
+    set -x GOPATH $HOME/Documents/Projets/go
+    set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-13.0.2.jdk/Contents/Home
+    set PATH $FLUTTERPATH/bin $GOPATH/bin $PATH
 end
 
 starship init fish | source
